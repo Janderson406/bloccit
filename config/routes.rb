eb59840f7bf48c2d1dfce9445349003621efb09e
Jon Anderson <jondanderson@gmail.com>
2016-01-19 21:36:49 -0700

    Assignment 26 Testing: Completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/faq'
+
   root 'welcome#index'
   #root method allows us to declare the default page the app loads when we navigate to the home page URL.
   #root is a method that takes a hash as an argument

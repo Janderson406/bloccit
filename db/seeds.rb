b50a4e0cc873c39f7158e82290456cb4522fd804
Jon Anderson <jondanderson@gmail.com>
2016-01-26 19:05:56 -0700

    Checkpoint 30 Seeding Data: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create Posts
+50.times do
+  Post.create!(
+  #! instructs the method to raise error if there's a problem with data we're seeding.
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+# Create Comments
+100.times do
+  Comment.create!(
+#call sample on the array returned by Post.all,
+#in order to pick a random post to associate each comment with
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
---
+++ lib/random_data.rb
@@ -0,0 +1,34 @@
+module RandomData
+ #define as module because it's a standalone library w/ no dependencies/inheritance requirements
+   def self.random_paragraph
+     sentences = []
+     rand(4..6).times do
+       sentences << random_sentence
+     end
+     #join combines each sentence into a full paragraph (as a string).
+     sentences.join(" ")
+   end
+
+ # #7
+   def self.random_sentence
+     strings = []
+     rand(3..8).times do
+       strings << random_word
+     end
+
+     sentence = strings.join(" ")
+     sentence.capitalize << "."
+     #capitalize returns a copy of sentence with the first character
+     #converted to uppercase and the remainder of the sentence converted to lowercase
+   end
+
+ # #8
+   def self.random_word
+     letters = ('a'..'z').to_a #set letters to an array
+     letters.shuffle!
+     #call shuffle! on letters in place. If we were to call shuffle without the bang (!)
+     #then shuffle would return an array rather than shuffle in place
+     letters[0,rand(3..8)].join #join the zeroth through nth item in letters
+                #The nth item is the result of rand(3..8)
+   end
+ end

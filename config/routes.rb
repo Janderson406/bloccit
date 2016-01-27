12b71ca61996b76abac7b68d18fd3aba46e5da5e
Jon Anderson <jondanderson@gmail.com>
2016-01-26 22:40:17 -0700

    Checkpoint 31 CRUD: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  #get 'posts/index'
+  #get 'posts/show'
+  #get 'posts/new'
+  #get 'posts/edit'
+#==> REFACTORED TO:
+  resources :posts
+    #instructs Rails to create post routes for creating, updating,
+    #viewing, and deleting instances of Post
 
-  get 'welcome/about'
+  #get 'welcome/index'
+  #get 'welcome/about'
+#==> REFACTORED TO:
+  get 'about' => 'welcome#about'
+  #1. remove get "welcome/index" b/c we've declared index view as the root view
+  #2. modify about route; allows users to visit /about, rather than /welcome/about
+
 
   root 'welcome#index'
   #root method allows us to declare the default page the app loads when we navigate to the home page URL.

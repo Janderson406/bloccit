ca87ff5ca83aebd0ff142694c3c3a7859ec4bb19
Jon Anderson <jondanderson@gmail.com>
2016-01-27 22:29:13 -0700

    Checkpoint 32 CRUD pt2: completed

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -13,26 +13,77 @@ RSpec.describe PostsController, type: :controller do
 
     it "assigns [my_post] to @posts" do
       get :index
-#because our test created a post (my_post), expect index to return an array of one item. 
+#test created a post (my_post), expect index to return an array of one item.
 #use assigns, a method in ActionController::TestCase. assigns gives the test access
 #to "instance variables assigned in the action that are available for the view".
       expect(assigns(:posts)).to eq([my_post])
     end
   end
 
- #  describe "GET show" do
- #    it "returns http success" do
- #      get :show
- #      expect(response).to have_http_status(:success)
- #    end
- #  end
+###READING POST TESTS
+ describe "GET show" do
+     it "returns http success" do
+       get :show, {id: my_post.id}
+       expect(response).to have_http_status(:success)
+     end
+       #pass {id: my_post.id} to show as a parameter. These parameters are passed to the params hash.
+       #The params hash contains all parameters passed to the application's controller
+       #(application_controller.rb), whether from GET, POST, or any other HTTP action.
 
- # describe "GET new" do
- #   it "returns http success" do
- #     get :new
- #     expect(response).to have_http_status(:success)
- #   end
- # end
+     it "renders the #show view" do
+       get :show, {id: my_post.id}
+       expect(response).to render_template :show
+     end
+     #expect the response to return the show view using the render_template matcher.
+
+     it "assigns my_post to @post" do
+       get :show, {id: my_post.id}
+       expect(assigns(:post)).to eq(my_post)
+     end
+     # expect the post to equal my_post because we call show with the id of my_post.
+     # We are testing that the post returned to us is the post we asked for.
+ end
+
+###CREATING POSTS TESTS: NEW & CREATE
+  #When *new* is invoked, a new and unsaved Post object is created.
+  #When *create* is invoked, the newly created object is persisted to the database
+ describe "GET new" do
+      it "returns http success" do
+        get :new
+        expect(response).to have_http_status(:success)
+      end
+
+      it "renders the #new view" do
+        get :new
+        expect(response).to render_template :new
+      end
+      #we expect PostsController#new to render the posts new view
+
+      it "instantiates @post" do
+        get :new
+        expect(assigns(:post)).not_to be_nil
+      end
+    end
+    #we expect @post instance variable to be initialized by PostsController#new.
+    #assigns gives us access to the @post variable, assigning it to :post.
+
+    describe "POST create" do
+      it "increases the number of Post by 1" do
+        expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+      end
+      #after PostsController#create is called, Post instances (i.e. rows in the posts table) in the database will increase
+
+      it "assigns the new post to @post" do
+        post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+        expect(assigns(:post)).to eq Post.last
+      end
+
+      it "redirects to the new post" do
+        post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+        expect(response).to redirect_to Post.last
+      end
+    end
+    #we expect to be redirected to the newly created post.
 
  #  describe "GET edit" do
  #    it "returns http success" do

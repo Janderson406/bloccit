52d1a1b87bda87035f6ce8484a901374868ab6b8
Jon Anderson <jondanderson@gmail.com>
2016-02-03 00:05:19 -0700

    Assignment 34 Topics/Sponsored Posts: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   #get 'posts/index'
   #get 'posts/show'
   #get 'posts/new'
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
 #resource instructs Rails to create routes for creating, updating, viewing, and deleting instances
 resources :topics do
    resources :posts, except: [:index]
+   resources :sponsored_posts, except: [:index]
  end
   #^^ we pass resources :posts to the resources :topics block. This nests the post routes under the topic routes.
   #posts index view is no longer needed. All posts will be displayed with respect to a topic now, on the topics show view
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,17 @@ topics = Topic.all
 end
 posts = Post.all
 
+# Create SponsoredPosts
+50.times do
+  SponsoredPost.create!(
+    topic:  topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph,
+    price:  1000
+  )
+end
+sponsored_posts = SponsoredPost.all
+
 # Create Comments
 100.times do
   Comment.create!(
@@ -31,4 +42,5 @@ end
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
+puts "#{SponsoredPost.count} sponsored_posts created"
 puts "#{Comment.count} comments created"

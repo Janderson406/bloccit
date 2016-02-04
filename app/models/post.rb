3fd5b7e3483f6ce54aef1d0913f4388f65172247
Jon Anderson <jondanderson@gmail.com>
2016-02-03 20:00:05 -0700

    Checkpoint 35 Validation: completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,14 @@
 class Post < ActiveRecord::Base #handles interaction with database/allows us to persist data through our class
   belongs_to :topic
   has_many :comments, dependent: :destroy
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
 end
 #Comments are dependent on a post's existence because of the has_many :comments declaration in Post.
 #When we delete a post, we also need to delete all related comments.
 #perform a "cascade delete" => ensures that when a post is deleted, all of its comments are too.
+
+
+#test validation   n = Post.new :title => "A Very Long Title", :body => "this is the body. where these characters live", :topic => "validations"
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe Post, type: :model do
   it { is_expected.to belong_to(:topic) }
   #we associate post with topic via topic.posts.create!, chained method call which creates a post for a given topic.
 
+   it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:body) }
+   it { is_expected.to validate_presence_of(:topic) }
+ 
+   it { is_expected.to validate_length_of(:title).is_at_least(5) }
+   it { is_expected.to validate_length_of(:body).is_at_least(20) }
+
   describe "attributes" do
      #we test whether post has an attribute named title. This tests whether post will return a non-nil value when post.title is called.
      it "should respond to title" do

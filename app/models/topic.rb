a60b24d48f59b40fedc3f1790f2713c62eda68cf
Jon Anderson <jondanderson@gmail.com>
2016-03-02 21:25:33 -0700

    Assignment 46 Private Topics: completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -8,7 +8,13 @@ class Topic < ActiveRecord::Base
    #scope :visible_to, -> { where(public: true) }
    # ^ This partially works, but it doesn't solve the problem of determining whether the topic is visible to a *particular user.*
 
-   scope :visible_to, -> (user) { user ? all : where(public: true) }
+   scope :visible_to, -> (user) { user ? all : Topic.publicly_viewable }
    #the ternary operator "?" keeps the lambda "->" on one line. This is essentially a one-line if / else condition.
    #It now returns the equivalent of topic_collection.all or topic_collection.where(public: true), depending on the value of user.
+
+   scope :publicly_viewable, -> {where(public: true)}
+
+   scope :privately_viewable, -> {where(public: false)}
+
+
 end

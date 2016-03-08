da8300ea7f43a4ca4c4e9fb07d2cd0286955cf71
Jon Anderson <jondanderson@gmail.com>
2016-03-07 22:14:35 -0700

    Checkpoint 47 Ajax: completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,32 +5,44 @@ class CommentsController < ApplicationController
 
    def create
      @post = Post.find(params[:post_id])
-     comment = @post.comments.new(comment_params)
-     comment.user = current_user
+     @comment = @post.comments.new(comment_params)
+     @comment.user = current_user
      #find the correct post using post_id and then create a new comment using comment_params. We assign the comment's user to current_user, which returns the signed-in user instance.
+     @new_comment = Comment.new
+      #render a new form partial, with a new instance of a comment to clear the comment form, then use the new variable in the create.js.erb file
 
-     if comment.save
+     if @comment.save
        flash[:notice] = "Comment saved successfully."
-       redirect_to [@post.topic, @post]
-       #redirect to the posts show view. Depending on whether the comment was valid, we'll either display a success or an error message to the user.
+
      else
        flash[:alert] = "Comment failed to save."
-       redirect_to [@post.topic, @post]
+
+     end
+
+     respond_to do |format|
+       format.html
+       format.js
      end
    end
 
 
    def destroy
      @post = Post.find(params[:post_id])
-     comment = @post.comments.find(params[:id])
-
-     if comment.destroy
+     @comment = @post.comments.find(params[:id])
+     #replace comment with @comment because we'll need to have access to the variable in our .js.erb view
+     if @comment.destroy
        flash[:notice] = "Comment was deleted."
-       redirect_to [@post.topic, @post]
      else
        flash[:alert] = "Comment couldn't be deleted. Try again."
-       redirect_to [@post.topic, @post]
      end
+
+     respond_to do |format|
+       format.html
+       format.js
+     end
+      #the respond_to block gives our controller action the ability to return different response types,
+      #depending on what was asked for in the request. The controller's response is unchanged if the
+      #client requests HTML, but if the client requests JavaScript, the controller will render .js.erb instead.
    end
 
 
